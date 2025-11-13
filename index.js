ccd3ecd6522354ebd26a5cfe2a316e5a2fc0ec08
SohelRana450 <msr945071337@gmail.com>
2025-11-13 23:39:51 +0600

    last final commit

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const admin = require("firebase-admin");
-const serviceAccount = require("./project.json");
 
 const app = express()
 const port = process.env.port || 3000
@@ -12,29 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
-const verifyToken = async (req, res, next) =>{
-    const authorization = req.headers.authorization
-    if(!authorization){
-        res.status(401).send({
-            message: "unauthorization access."
-        })
-    }
-    const token = authorization.split(' ')[1]
 
-    try {
-        await admin.auth().verifyIdToken(token) 
-        next()
-    } catch (error) {
-        res.status(401).send({
-            message: "unauthorization access."
-        })
-    }
-   
-}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster01.aptihsx.mongodb.net/?appName=Cluster01`;
 
@@ -54,32 +32,32 @@ async function run() {
     const addLikesCollection = addArtwork.collection('likes')
     const addFavoritesCollection = addArtwork.collection('favoriteArt')
 
-    app.post("/addArtwork",verifyToken,async (req,res)=>{
+    app.post("/addArtwork",async (req,res)=>{
         const newUser = req.body;
         const result = await addArtworkCollection.insertOne(newUser);
         res.send(result)
     })
-    app.get("/addArtwork", verifyToken, async (req, res)=>{
+    app.get("/addArtwork", async (req, res)=>{
         const cursor = await addArtworkCollection.find().toArray();
         res.send(cursor)
 
     })
 
-    app.get('/addArtwork/:id', verifyToken, async (req,res) => {
+    app.get('/addArtwork/:id',  async (req,res) => {
         const id = req.params.id
         const query = { _id: new ObjectId(id)};
         const result = await addArtworkCollection.findOne(query);
         res.send(result)
     })
 
-    app.get('/latest-addArtwork',verifyToken, async (req ,res)=>{
+    app.get('/latest-addArtwork', async (req ,res)=>{
         const result = await addArtworkCollection.find().sort({createdAt: 'desc'}).limit(6).toArray();
 
         res.send(result)
     })
     
     
-app.post("/likes/:id", verifyToken, async (req, res) => {
+app.post("/likes/:id",  async (req, res) => {
   const { userEmail } = req.body; 
   const id = req.params.id;
   const filter = { _id: new ObjectId(id) };
@@ -105,26 +83,26 @@ app.post("/likes/:id", verifyToken, async (req, res) => {
 });
 
 
-    app.post('/favoriteArt', verifyToken, async (req,res) => {
+    app.post('/favoriteArt',  async (req,res) => {
         const data = req.body;
     const result = await addFavoritesCollection.insertOne(data)
         res.send(result)
     })
 
-    app.get('/my-favoriteArt',verifyToken, async (req, res) => {
+    app.get('/my-favoriteArt', async (req, res) => {
         const email = req.query.email
         const result = await addFavoritesCollection.find({favorite_by: email, }).toArray()
         res.send(result)
 
     })
-    app.get('/favoriteArt/:id',verifyToken, async (req ,res) =>{
+    app.get('/favoriteArt/:id', async (req ,res) =>{
         const id = req.params.id
         const query = {_id: id}
         const result = await addFavoritesCollection.findOne(query);
         res.send(result)
     })
 
-    app.delete('/favoriteArt/:id', verifyToken, async (req, res) =>{
+    app.delete('/favoriteArt/:id', async (req, res) =>{
         
         const id = req.params.id
         const filter = ({ _id: id})
@@ -132,13 +110,13 @@ app.post("/likes/:id", verifyToken, async (req, res) => {
         res.send(result)
     })
 
-    app.get('/my-gallery',verifyToken, async (req, res) =>{
+    app.get('/my-gallery', async (req, res) =>{
         const email = req.query.email
         const result = await addArtworkCollection.find({email: email}).toArray()
         res.send(result)
     })
 
-    app.put('/addArtwork/:id',verifyToken, async (req,res) =>{
+    app.put('/addArtwork/:id', async (req,res) =>{
         const id = req.params.id;
         const data = req.body;
         const filter = { _id: new ObjectId(id)}
@@ -149,14 +127,14 @@ app.post("/likes/:id", verifyToken, async (req, res) => {
         res.send(result)
     })
 
-    app.delete('/addArtwork/:id', verifyToken, async (req, res) =>{
+    app.delete('/addArtwork/:id', async (req, res) =>{
         const id = req.params.id;
         const filter = { _id: new ObjectId(id)};
         const result = await addArtworkCollection.deleteOne(filter);
         res.send(result)
     })
 
-    app.get('/search', verifyToken, async (req,res) =>{
+    app.get('/search', async (req,res) =>{
         const search = req.query.search 
         const result = await addArtworkCollection.find({ title: {$regex: search, $options: "i"}}).toArray()
         res.send(result)
